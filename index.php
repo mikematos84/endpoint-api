9506e1341f288dcecda38a5b3bab70c308af4153
Michael Matos <mikematos84@gmail.com>
2017-03-29 11:10:56 -0400

    Fixed file location errors

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
     require __DIR__ . '/response.php';
 
     // Connect to a database using ADOdb
